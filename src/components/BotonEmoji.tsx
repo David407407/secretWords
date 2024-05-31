13303639a4a826846fcdf3258bf31cddaa77c469
David407407 <darksideinthespace@gmail.com>
2024-05-31 12:19:49 -0600

    first commit

--- src/components/BotonEmoji.tsx
+++ src/components/BotonEmoji.tsx
@@ -6,7 +6,7 @@ const BotonEmoji = (props: Props) => {
     
 
   return (
-    <button className="w-full text-white outfit font-bold lg:text-lg bg-[#D9324F] hover:scale-105 transition-all rounded-xl px-6 py-3 flex items-center justify-center gap-4">
+    <button className="w-36 lg:w-full text-white outfit font-bold lg:text-lg bg-[#D9324F] hover:scale-105 transition-all rounded-xl px-6 py-3 flex items-center justify-center gap-4">
         <p>{"😆"}</p> 
         Emociones
     </button>
