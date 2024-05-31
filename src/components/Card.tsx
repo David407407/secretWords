deff902a205a7f9927dd276c12b0866a73cf167d
David407407 <darksideinthespace@gmail.com>
2024-05-31 11:52:43 -0600

    first commit

---
+++ src/components/Card.tsx
@@ -0,0 +1,43 @@
+import React, { useState } from 'react'
+import { getRandomNumber } from '../helpers/getRandomNumber';
+
+type Props = {
+    words: any,
+}
+
+const Card = (props: Props) => {
+    const { words } = props
+    const [ random, setRandom ] = useState(getRandomNumber(words.length-1))
+
+    const { palabra, significado, dato, idioma } = words[random]
+
+  return (
+    <>
+        <div className="w-2/3 mx-auto rounded-3xl bg-[#A99A89] p-10">
+            <h1 className="text-6xl font-black text-right pb-4">{palabra}</h1>
+
+            <div className="w-11/12 mx-auto bg-black h-[2px]"></div>
+
+            <div className="w-4/5 mx-auto py-6 space-y-6">
+                <p className="text-2xl font-medium text-center">{significado}</p>
+                <p className="text-3xl font-medium"> <span className="text-white">Idioma</span> - {idioma}</p>
+                {
+                    dato !== palabra && (
+                        <p className="text-3xl font-medium"> <span className="text-white">Dato</span> - { dato }</p>
+                    )
+                }
+                
+            </div>
+        </div>
+
+        <div className="flex justify-between gap-80 my-24">
+            <button onClick={() => setRandom(getRandomNumber(words.length-1))} className="w-40 mx-auto text-white outfit font-bold text-lg bg-[#D9324F] hover:scale-105 transition-all rounded-xl px-6 py-3 flex items-center justify-center gap-4">
+                Otra palabra
+            </button>
+        </div>
+    </>
+    
+  )
+}
+
+export default Card
